add5c7e1d16c266d083232dfdbed7823ba3a7903
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-03 18:39:07 +0500

    Time: 3 ms (83.19%) | Memory: 10.8 MB (91.43%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,21 +3,24 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
         
+        // Iterate over each element in nums1
         for (int i = 0; i < nums1.size(); ++i) {
             int nextGreater = -1;
             bool found = false;
             
+            // Iterate over nums2 to find the position of nums1[i] and the next greater element
             for (int j = 0; j < nums2.size(); ++j) {
                 if (nums2[j] == nums1[i]) {
-                    found = true;
+                    found = true;  // Mark that we found nums1[i] in nums2
                 }
                 
                 if (found && nums2[j] > nums1[i]) {
-                    nextGreater = nums2[j];
+                    nextGreater = nums2[j];  // Find the next greater element
                     break;
                 }
             }
             
+            // Add the next greater element (or -1 if there isn't one) to the result vector
             result.push_back(nextGreater);
         }
         
