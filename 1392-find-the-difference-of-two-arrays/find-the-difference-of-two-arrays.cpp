56fbb22dd4b20ee724b78e39e75b03164c014d6a
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-04 05:20:15 +0500

    Time: 40 ms (61.96%) | Memory: 40.5 MB (6.54%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-   vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-    unordered_set<int> set1(nums1.begin(), nums1.end());
-    unordered_set<int> set2(nums2.begin(), nums2.end());
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
 
-    vector<int> diff1, diff2;
+        vector<int> diff1;
+        vector<int> diff2;
 
-    // Find elements in nums1 but not in nums2
-    for (const int& num : set1) {
-        if (set2.find(num) == set2.end()) {
-            diff1.push_back(num);
-        };
-    };
+        // We use a set to track added elements to avoid duplicates in the result
+        unordered_set<int> added1;
+        unordered_set<int> added2;
 
-    // Find elements in nums2 but not in nums1
-    for (const int& num : set2) {
-        if (set1.find(num) == set1.end()) {
-            diff2.push_back(num);
-        };
-    };
+        // Find elements in nums1 but not in nums2
+        for (const int& num : nums1) {
+            if (set2.find(num) == set2.end() && added1.find(num) == added1.end()) {
+                diff1.push_back(num);
+                added1.insert(num);
+            }
+        }
 
-    return {diff1, diff2};
-};
+        // Find elements in nums2 but not in nums1
+        for (const int& num : nums2) {
+            if (set1.find(num) == set1.end() && added2.find(num) == added2.end()) {
+                diff2.push_back(num);
+                added2.insert(num);
+            }
+        }
+
+        return {diff1, diff2};
+    }
 };
