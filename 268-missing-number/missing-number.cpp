fbe9aefeadd5898106478f8cc0e64a63cf1615a1
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-04 07:08:35 +0500

    Time: 19 ms (24.15%) | Memory: 27.7 MB (5.01%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+   unordered_set<int> numSet(nums.begin(), nums.end());
+    int n = nums.size();
+    
+    for (int i = 0; i <= n; ++i) {
+        if (numSet.find(i) == numSet.end()) {
+            return i; // Missing number found
+        }
+    }
+    
+    return -1;
+        
+    }
+};
