90f5a021328502597945b201afb3477bedba423b
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-04 15:55:36 +0500

    Time: 92 ms (58.36%) | Memory: 73.2 MB (44.09%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hashset;
+        for (const int& n : nums) {
+            if (hashset.find(n) != hashset.end()) {
+                return true;
+            }
+            hashset.insert(n);
+        }
+        return false;
+    }
+};
