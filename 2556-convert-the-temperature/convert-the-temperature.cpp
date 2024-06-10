90c97f057f529f70e191e778a800d3252e6874c4
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-10 20:19:40 +0500

    Time: 3 ms (44.63%) | Memory: 7.5 MB (74.31%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<double> convertTemperature(double celsius) {
-    
+    vector<double> convertTemperature(double celsius) {  
       return {celsius + 273.15,(celsius * 9/5) + 32};
-    }
+    };
 };
