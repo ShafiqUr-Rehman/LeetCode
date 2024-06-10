0882c9d1243a9abed54d0dff74b4b9610de93b1e
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-11 02:25:58 +0500

    Time: 11 ms (41.43%) | Memory: 8.3 MB (48.02%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        if (x < 0) {
+            return false;
+        };
+        long int sub = x;
+        long int rev_num = 0;
+        while (x > 0) {
+            int pop = x % 10; 
+            x /= 10; 
+            rev_num = rev_num * 10 + pop;
+        };
+        // Check if the original number is equal to the reversed number
+        return sub == rev_num;  // MT
+    }
+};
