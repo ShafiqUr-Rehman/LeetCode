3bd7b6a68a63a284b54c72471e98087c1553ad48
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-11 02:12:06 +0500

    Time: 3 ms (55.40%) | Memory: 8.3 MB (74.24%) - LeetSync

---
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+      // Example  num = 4 and t=1(it means thar i can just increment or decrement just onr time)
+      // let output => x = 6;
+      // x-1 = 5  ...... num+1 = 5  true;
+      
+      //Another Example : num=3  and t=2 (i can use increment or decrement 2-Times)
+      //x = 7    && num = 3 
+      // 7-1 = 6  && 3+1 = 4
+      // 6-1 =5  **  4+1 = 5;
+      // now lets fint hte valus of x
+      // x-t = num + t    (t menas times)
+      // x=num + t + t;
+      // x = num + 2t;
+      return num + 2*t;
+      
+    };
+};
