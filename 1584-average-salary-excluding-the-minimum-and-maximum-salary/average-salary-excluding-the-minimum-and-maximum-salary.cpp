e422e8a22cfc31c2407d158cf022cfb46aaa4e73
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-12 12:41:00 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (27.78%) - LeetSync

---
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+    
+     sort(salary.begin(),salary.end());
+        double sum = 0;
+        int count =0;
+        double avg = 0.0; //Perform calculations using floating-point arithmetic to maintain precision.
+        for(int i=1;i<salary.size()-1;i++){
+            sum=sum+salary[i];
+            count++;
+        }
+      
+        avg = sum/count;
+        return avg;
+    }
+};
