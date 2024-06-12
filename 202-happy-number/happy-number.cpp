9c1ab5ca2e732d4436187f143ecf65fce13343f2
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-12 18:19:59 +0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (26.72%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -4,19 +4,20 @@ class Solution {
     int sum=0;
   while(n!=0){
         
-        int rem=n%10;
-        sum+=rem*rem;
-        n/=10;
+        int rem = n % 10;  // Get the last digit
+        sum += rem * rem;  // Add the square of the last digit to sum
+        n /= 10;           // Remove the last digit
         }
         return sum;
     }
 public:
     bool isHappy(int n) {
-    unordered_set<int>s;
-      while(n!=1 && !s.count(n)){
-        s.insert(n);
-        n=cal(n);
-      }
-        return n==1;
+   unordered_set<int> s;
+   //Continue the loop until n becomes 1 (indicating a happy number) or n is found in the set (indicating a cycle).
+    while (n != 1 && !s.count(n)) {
+        s.insert(n);   // Insert current number into the set
+        n = cal(n);    // Calculate the next number in the sequence
+    }
+    return n == 1;
     }
 };
