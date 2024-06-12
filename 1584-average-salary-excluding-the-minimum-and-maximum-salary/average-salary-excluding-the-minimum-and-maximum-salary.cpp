b0d6df3328d00b11f23db394230d620e0a1cd74a
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-12 12:42:26 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (27.78%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -8,8 +8,7 @@ public:
         for(int i=1;i<salary.size()-1;i++){
             sum=sum+salary[i];
             count++;
-        }; 
-            
+        };         
         avg = sum/count;
         return avg;
     };
