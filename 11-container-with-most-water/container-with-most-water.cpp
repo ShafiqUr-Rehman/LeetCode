8ccae9275ea5c2aeeacad6fdfb73388def2163e6
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-26 17:53:13 +0500

    Time: 53 ms (89.37%) | Memory: 61.4 MB (26.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -6,19 +6,16 @@ public:
         int right = height.size() - 1;
         
         while (left < right) {
-            // Calculate current area
-            int currentArea = (right - left) * min(height[left], height[right]);
-            // Update maxArea if current area is larger
+            int currentArea = (right - left)*min(height[left], height[right]);
             maxArea = max(maxArea, currentArea);
-            
-            // Move the pointer pointing to the smaller height
+
             if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
-        };
+        }
         
         return maxArea;
-    };
+    }
 };
