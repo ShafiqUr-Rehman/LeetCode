6c49e4a657d72e622c55243e8dd861e95533f1f1
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-06-29 14:36:33 +0500

    Time: 17 ms (70.62%) | Memory: 20.6 MB (47.73%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -7,15 +7,16 @@ private:
 public:
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        prefixSums.resize(n + 1);
+        prefixSums.resize(n + 1, 0);
         prefixSums[0] = 0; // Base case for prefix sum
         
-        for (int i = 0; i < n; ++i) {
-            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        for (int i = 1; i <= n; ++i) { // Start from 1 for 1-based indexing in prefixSums
+            prefixSums[i] = prefixSums[i - 1] + nums[i - 1];
         }
-    };
+    }
     
     int sumRange(int left, int right) {
-        return prefixSums[right + 1] - prefixSums[left];
-    };
+        return prefixSums[right + 1] - prefixSums[left]; // here right+1
+        // we use right+1 to handle 1-indexed base in prefixSums
+    }
 };
