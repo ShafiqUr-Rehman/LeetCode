47560bed3270ab91c548f97c4800551a3fd7bed9
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-07-01 23:24:09 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (86.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
+
         for (char ch : s) { // Simplified for loop
             if (st.empty()) {
                 st.push(ch);
+
             } else if ((ch == ')' && st.top() == '(') ||
                        (ch == ']' && st.top() == '[') ||
                        (ch == '}' && st.top() == '{')) {
                 st.pop();
+                
             } else {
                 st.push(ch);
             }
         }
-        if(st.empty()) return true;
+        if (st.empty())
+            return true;
         return false;
-    }
+    };
 };
