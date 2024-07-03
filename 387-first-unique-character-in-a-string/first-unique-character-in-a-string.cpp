1512fffb793d44a6b30eeccd56ec089c56958f0a
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-07-03 19:04:36 +0500

    Time: 46 ms (31.82%) | Memory: 14.1 MB (13.71%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         unordered_map<char, int> charCount;
-        
         // Count the occurrences of each character
         for (char c : s) {
             charCount[c]++;
-        }
-              
+        };
         // Find the first unique character
         for (int i = 0; i < s.size(); i++) {
             if (charCount[s[i]] == 1)
                 return i;
-        }
-        
+        };
         return -1;
-    }
+    };
 };
