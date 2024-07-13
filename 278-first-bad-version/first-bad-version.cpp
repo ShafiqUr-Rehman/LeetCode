88c83793102e2057fc256215d074a88d8119a99b
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-07-13 17:41:53 +0500

    Time: 2 ms (58.01%) | Memory: 7.5 MB (15.65%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -7,10 +7,10 @@ public:
         int left = 1;
         int right = n;
         
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
-                right = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
