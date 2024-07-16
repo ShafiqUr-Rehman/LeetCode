d62c65e961aeaa420960a48da01c7b1e721f238f
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-07-16 14:53:11 +0500

    Time: 52 ms (95.51%) | Memory: 45.2 MB (49.19%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,24 +1,28 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
+        unordered_map<int, int> prefixSumCount;
+        prefixSumCount[0] = 1; // To handle the case when the subarray starts from the beginning
+        
+        int currentSum = 0;
         int count = 0;
-
-        for (int start = 0; start < n; ++start) {
+        
+        for (int num : nums) {
+            currentSum += num;
             
-            int sum = 0;
-            for (int end = start; end < n; ++end) {
-                sum += nums[end];
-
-                if (sum == k) {
-                    count++;
-                }
+            // Check if there's a prefix subarray we can subtract to get sum k
+            if (prefixSumCount.find(currentSum - k) != prefixSumCount.end()) {
+                count += prefixSumCount[currentSum - k];
             }
+            
+            // Record the current sum in the map
+            prefixSumCount[currentSum]++;
         }
-
+        
         return count;
     }
 };
