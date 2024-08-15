0068399519b6c5c925510de3a5b030f3baee8373
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-08-15 06:26:14 +0500

    Time: 17 ms (40.43%) | Memory: 18.7 MB (29.69%) - LeetSync

--- 946-smallest-range-ii/smallest-range-ii.cpp
+++ 946-smallest-range-ii/smallest-range-ii.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int smallestRangeII(vector<int>& nums, int k) {
         int n = nums.size();
+         if (n == 1) return 0;
+         
         sort(nums.begin(), nums.end());        
-        int minScore = nums[n-1] - nums[0]; // Initial score (max - min)       
-        for (int i = 0; i < n - 1; i++) {
+        int minScore = nums[n-1] - nums[0];     
+       for (int i = 0; i < n - 1; i++) {
             int high = max(nums[n-1] - k, nums[i] + k);
             int low = min(nums[0] + k, nums[i+1] - k);
             minScore = min(minScore, high - low);
@@ -12,3 +14,5 @@ public:
         return minScore;
     }
 };
+
+
