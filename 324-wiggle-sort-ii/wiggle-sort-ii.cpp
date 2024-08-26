b20a48ea6f422f2e8c0b6c60c71b43d11bb3be85
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-08-26 17:01:54 +0500

    Time: 10 ms (80.68%) | Memory: 21 MB (41.46%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> sorted(nums);
-        sort(sorted.begin(), sorted.end());        
+        vector<int> temp(nums); 
+        sort(temp.begin(), temp.end());         
         int n = nums.size();
-        int j = (n + 1) / 2 - 1;  // Middle index for the smaller half
-        int k = n - 1;  // Last index for the larger half
+        int mid = (n + 1) / 2; // Middle index for the smaller half
+        int end = n; // Last index for the larger half        
+        // Fill the original array by placing the smaller elements in odd positions
+        // and the larger elements in even positions
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                nums[i] = sorted[j--]; 
+                nums[i] = temp[--mid];
             } else {
-                nums[i] = sorted[k--];  
+                nums[i] = temp[--end];
             }
         }
     }
