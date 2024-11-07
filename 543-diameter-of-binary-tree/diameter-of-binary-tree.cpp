9f589a62742a9f909cf6f57af6e937ad1a716824
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-11-07 10:10:32 +0500

    Time: 0 ms (100.00%) | Memory: 22.5 MB (57.55%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,7 +10,7 @@ private:
         if (!node) return 0; 
         int leftHeight = height(node->left);
         int rightHeight = height(node->right);
-        diameter = std::max(diameter, leftHeight + rightHeight);
-        return 1 + std::max(leftHeight, rightHeight);
+        diameter = max(diameter, leftHeight + rightHeight);
+        return 1 + max(leftHeight, rightHeight);
     }
 };
