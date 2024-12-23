5d7d09f165187314a9f7902dc8d0c394a1c16d70
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2024-12-23 09:11:27 +0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (54.48%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -3,12 +3,12 @@ public:
     int findNumbers(vector<int>& nums) {
         int evenCount = 0; 
         for (int num : nums) {
-            int digitCount = 0;
-            while (num > 0) {
-                num /= 10;
+            int digitCount =0;
+            while (num> 0) {
+                num /=10;
                 digitCount++;
             }
-            if (digitCount % 2 == 0) { 
+            if (digitCount % 2 ==0) { 
                 evenCount++;
             }
         }
