b6a44531cf032e19907016cdc45df516f96c8447
Shafiq Ur Rehman <119164431+ShafiqUr-Rehman@users.noreply.github.com>
2025-01-04 08:12:28 +0500

    Time: 0 ms (100.00%) | Memory: 11.2 MB (78.32%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> result(n);
+        vector<string> fizzBuzz(n);
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                result[i -1] = "FizzBuzz";
+                fizzBuzz[i - 1] = "FizzBuzz";
             } else if (i % 3 == 0) {
-                result[i -1] = "Fizz";
+                fizzBuzz[i - 1] = "Fizz";
             } else if (i % 5 == 0) {
-                result[i -1] = "Buzz";
+                fizzBuzz[i - 1] = "Buzz";
             } else {
-                result[i -1] = to_string(i);
+                fizzBuzz[i - 1] = to_string(i); // No need to use to_string here
             }
-        };
-        return result;
+        }
+        return fizzBuzz;
     }
 };
+
