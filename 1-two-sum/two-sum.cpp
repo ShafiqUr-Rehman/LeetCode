94a84194bf6a6f48763976ec0cced1cbe7ac23f5
Shafiq Ur Rehman <119164431+im-shafiqurrehman@users.noreply.github.com>
2025-01-19 14:25:19 +0500

    Time: 3 ms (75.41%) | Memory: 14.9 MB (20.07%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> complementMap;
+    unordered_map<int, int> map;
 
         for (int i = 0; i < nums.size(); ++i) {
-           // a= target -b     =>   a+b = target
             int a = target - nums[i];
 
-     if (complementMap.find(a) != complementMap.end()) {
-                return {complementMap[a], i};
+     if (map.find(a) != map.end()) {
+                return {map[a], i};
             };
             // Store current number and its index in the hashmap
-            complementMap[nums[i]] = i;   // both are index
+            map[nums[i]] = i;   // both are index
         };
-
-
         // If no solution found, return an empty vector
         return {};
     };
@@ -23,4 +20,4 @@ public:
 
 // firstly hashmap is empty ;
 // it store the values one by one;
-//if hi finds the value that is already exixts it is gurnatee that thier sum will br equal to target
+//if it finds the value that is already exists it is guarentee that thier sum will be equal to target
