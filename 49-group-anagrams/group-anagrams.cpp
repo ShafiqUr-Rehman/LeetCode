648d14b00302ff72384ea64dc91f5cdcf05c6809
Shafiq Ur Rehman <119164431+im-shafiqurrehman@users.noreply.github.com>
2025-01-22 19:07:37 +0500

    Time: 16 ms (65.41%) | Memory: 24.8 MB (88.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,14 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anagrams;
         for (string& s : strs) {
-            // Create a key by sorting the string
             string key = s;
             sort(key.begin(), key.end());
-            // Group the original string by its sorted version
             anagrams[key].push_back(s);
         }
-        
-        // Prepare the result vector
         vector<vector<string>> result;
         for (auto& pair : anagrams) {
             result.push_back(pair.second);
